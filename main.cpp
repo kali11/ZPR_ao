acf6efd492dd989628bddffa6c89545e516ec034
Piotr Kalinowski <kali.piotrek@gmail.com>
2013-04-24 19:38:45 +0200

    od nowa

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <boost/regex.hpp>
+#include <iterator>
+#include <algorithm>
+
+using namespace std;
+int main(int argc,char *argv[])
+{
+	cout << "jesli to sie wyswietla tzn, ze dziala boost" << endl;
+
+	system("PAUSE");
+	return 0;
+}
